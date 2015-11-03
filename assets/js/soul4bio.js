f38a37260e46ccedcf3beba6ab618f08a58564cc
gara501 <gandres.ramirez@gmail.com>
2015-11-02 23:36:57 -0500

    Tables.

--- assets/js/soul4bio.js
+++ assets/js/soul4bio.js
@@ -18,7 +18,21 @@ var soul4bio = (function(){
 			method: '',
 			getIMC: function() {
 				this.height = this.height / 100;
-				return (this.weight / (this.height * this.height)).toFixed(2);
+				var imcData = {};
+				imcData.IMC = (this.weight / (this.height * this.height)).toFixed(2);
+				if (IMC <= 18.5) {
+					imcData.rating = 'Bajo Peso';
+				}
+				if ((IMC > 18.5) && (IMC <= 24.9)) {
+					imcData.rating = 'Peso Normal';
+				}
+				if (IMC <= 18.5) {
+					imcData.rating = 'Bajo Peso';
+				}
+				if (IMC <= 18.5) {
+					imcData.rating = 'Bajo Peso';
+				}
+				return imcData;
 			},
 			getWaistHeightIndex: function() {
 				return this.waist / this.height;
@@ -31,7 +45,17 @@ var soul4bio = (function(){
 					total = 495/(1.29579-0.35004(Math.log(this.waist+this.hip-this.neck))+0.22100(Math.log(this.height)))-450;
 				}
 				return total;
-			}
+			},
+			getMetabolicIndex: function(gender, weight, height, age) {
+				var tmb = 0;
+				if (this.gender === 1) {
+					tmb = ((10 * this.weight) + (6.25 * this.height) - (5 * this.age) + 5);
+				} else {
+					tmb = ((10 * this.weight) + (6.25 * this.height) - (5 * this.age) - 161);
+				}
+				return tmb;
+			},
+			getActivityIndex: function(activity)
 		};
 
 		//Actions
@@ -56,8 +80,8 @@ var soul4bio = (function(){
 			user.lastname = values.lastname;
 			user.age = values.age;
 			user.ppm = values.ppm;
-			user.faf = values.faf;
-			user.objective = values.objevtive;
+			user.activity = values.activity;
+			user.objective = values.objective;
 			user.nmet = values.nmet;
 
 			//Response values asignation
